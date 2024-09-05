1602e18a044bf766383c835e5b8f5873feb144b1
John Doe <johndoe@email.com>
2024-09-05 13:42:21 -0400

    Create and test calculateTip function

--- cc_2.js
+++ cc_2.js
@@ -11,3 +11,15 @@ let tip = (50 <= bill <= 300) ? bill * 0.15 : bill * 0.20;
 // Task 2: Output Details
 
 console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${bill + tip}`);
+
+
+
+// Task 3: Create a Function (calculateTip)
+
+function calculateTip(bill) {
+    return (50 <= bill <= 300) ? bill * 0.15 : bill * 0.20;
+}
+
+// Testing the function
+console.log(calculateTip(100));
+
