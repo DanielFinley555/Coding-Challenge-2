ae3c8e2e2f4073660f62dfe54516bf97d2397a82
John Doe <johndoe@email.com>
2024-09-05 14:11:43 -0400

    Implement and populate arrays for bills, tips, and totals

--- cc_2.js
+++ cc_2.js
@@ -10,7 +10,7 @@ let tip = (50 <= bill <= 300) ? bill * 0.15 : bill * 0.20;
 
 // Task 2: Output Details
 
-console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${bill + tip}`);
+console.log(`The bill was $${bill}, the tip was $${tip}, and the total value is $${bill + tip}`);
 
 
 
@@ -21,5 +21,17 @@ function calculateTip(bill) {
 }
 
 // Testing the function
-console.log(calculateTip(100));
+console.log("Tip for $100: $",calculateTip(100));
+
+
+
+// Task 4: Utilize Arrays
+
+const bills = [275, 40, 430];
+const tips = bills.map(bill => calculateTip(bill));
+const totals = bills.map((bill, index) => bill + tips[index]);
+
+console.log("Bills: ", bills);
+console.log("Tips: ", tips);
+console.log("Totals: ", totals);
 
