013180ffbce867c9a80fc51b052340723776115b
John Doe <johndoe@email.com>
2024-09-05 13:34:39 -0400

    "Add output functionality to display bill, tip, and total"

--- cc_2.js
+++ cc_2.js
@@ -5,3 +5,9 @@ let bill = 275;
 
 // Calculation using ternary operator
 let tip = (50 <= bill <= 300) ? bill * 0.15 : bill * 0.20;
+
+
+
+// Task 2: Output Details
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${bill + tip}`);
